0164358422615a340aab60c2cac603df1bcbeb37
MushroomBird <MushroomBirdHD@gmail.com>
2014-10-18 12:57:19 +0200

    Edited main class

--- src/com/mushroombird/devathlon/Devathlon.java
+++ src/com/mushroombird/devathlon/Devathlon.java
@@ -1,5 +1,7 @@
 package com.mushroombird.devathlon;
 
+import java.util.Random;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -8,13 +10,41 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class Devathlon extends JavaPlugin {
 	
+	//INSTANCE
+	private static Devathlon instance; 
+	
+	//RANDOM
+	private static Random random = new Random();
+	
+	@Override
+	public void onLoad() {
+		instance = this;
+	}
+	
 	@Override
 	public void onEnable() {
-		
+		getLogger().info( "Plugin aktiviert!" );
 	}
 	
 	@Override
 	public void onDisable() {
-		
+		getLogger().info( "Plugin deaktiviert!" );
+	}
+	
+	/**
+	 * Get Instance of mainclass
+	 * @return Instance of main class
+	 */
+	public static Devathlon getInstance() {
+		return instance;
+	}
+	
+	/**
+	 * Get Random
+	 * @see java.util.Random
+	 * @return Random
+	 */
+	public static Random getRandomEngine() {
+		return random;
 	}
 }
