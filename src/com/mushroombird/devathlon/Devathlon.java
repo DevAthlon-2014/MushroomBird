1c543282118d7ccd7bca7f0b58e0c5a54bf4464b
MushroomBird <MushroomBirdHD@gmail.com>
2014-10-18 11:18:02 +0200

    Created main class

---
+++ src/com/mushroombird/devathlon/Devathlon.java
@@ -0,0 +1,20 @@
+package com.mushroombird.devathlon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Devathlon plugin, created on 18.10.2014
+ * @author MushroomBird
+ */
+public class Devathlon extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
