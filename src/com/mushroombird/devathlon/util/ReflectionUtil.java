26d0b29873d29fee03f4cebaebdbd038ed916a01
MushroomBird <MushroomBirdHD@gmail.com>
2014-10-18 13:17:44 +0200

    Added stuff

--- src/com/mushroombird/devathlon/util/ReflectionUtil.java
+++ src/com/mushroombird/devathlon/util/ReflectionUtil.java
@@ -1,7 +1,14 @@
 package com.mushroombird.devathlon.util;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import com.mushroombird.devathlon.Devathlon;
+
+/**
+ * ReflectionUtil
+ * @author MushroomBird
+ */
 public class ReflectionUtil {
 	
 	/**
@@ -17,5 +24,32 @@ public class ReflectionUtil {
 		}
 		//IF CAN'T GET METHOD, RETURNS NULL
 		return null;
+	}
+	
+	/**
+	 * Get a Field
+	 */
+	public static Field getField(Class<?> clazz, String name) {
+		try {
+			return clazz.getDeclaredField(name);
+		} catch (NoSuchFieldException | SecurityException e) {
+			Devathlon.getInstance().getLogger().info("Can't get field! @see " + ReflectionUtil.class.getName() + ": Method: getFields");
+		}
+		
+		//IF CAN'T GET FIELD, RETURNS NULL
+		return null;
+	}
+	
+	/**
+	 * Set value
+	 */
+	public static void setValue(Class<?> clazz, String fieldName, Object object, Object value) {
+		Field field = getField(clazz, fieldName);
+		try {
+			field.setAccessible(true);
+			field.set(field, value);
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			Devathlon.getInstance().getLogger().info("Can't set value! @see " + ReflectionUtil.class.getName() + ": Method: setValue");
+		}
 	}
 }
